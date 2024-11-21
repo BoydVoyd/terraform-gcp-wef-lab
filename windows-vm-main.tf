9d6b8768be9c547dbdbfa62ee4b692a3c453d5c3
Nathan Davis <nathanwdavis@gmail.com>
2024-11-21 16:02:08 -0600

    Added WEC VM

--- windows-vm-main.tf
+++ windows-vm-main.tf
@@ -42,4 +42,34 @@ resource "google_compute_instance" "vm_instance_public" {
     network_ip    = "10.10.15.2"
     access_config { }
   }
+}
+
+# Create WEC  VM
+resource "google_compute_instance" "vm_instance_wec" {
+  # name         = "${lower(var.company)}-${lower(var.app_name)}-${var.environment}-vm${random_id.instance_id.hex}"
+  name = "lab-wec"
+  machine_type = var.windows_instance_type
+  zone         = var.gcp_zone
+  hostname     = "lab-wec.${var.app_domain}"
+  tags         = ["rdp","dc"]
+
+  boot_disk {
+    initialize_params {
+      image = var.windows_2016_sku
+    }
+  }
+
+  metadata = {
+    sysprep-specialize-script-ps1 = file("scripts/sysprep-specialize.ps1")
+  }
+
+  metadata_startup_script = file("scripts/startup-script.ps1")
+
+  network_interface {
+    network       = google_compute_network.vpc.name
+    subnetwork    = google_compute_subnetwork.network_subnet.name
+    network_ip    = "10.10.15.3"
+    access_config { }
+  }
 } 
+
