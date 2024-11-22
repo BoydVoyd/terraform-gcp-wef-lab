a824b76c4100e284d0509e1b634f247558a2172e
Nathan Davis <nathanwdavis@gmail.com>
2024-11-22 11:07:38 -0600

    added test server

--- windows-vm-main.tf
+++ windows-vm-main.tf
@@ -73,3 +73,31 @@ resource "google_compute_instance" "vm_instance_wec" {
   }
 } 
 
+# Create WEC  VM
+resource "google_compute_instance" "vm_instance_test" {
+  # name         = "${lower(var.company)}-${lower(var.app_name)}-${var.environment}-vm${random_id.instance_id.hex}"
+  name = "lab-test"
+  machine_type = var.windows_instance_type
+  zone         = var.gcp_zone
+  hostname     = "lab-test.${var.app_domain}"
+  tags         = ["rdp","dc"]
+
+  boot_disk {
+    initialize_params {
+      image = var.windows_2016_sku
+    }
+  }
+
+  metadata = {
+    sysprep-specialize-script-ps1 = file("scripts/sysprep-specialize.ps1")
+  }
+
+  metadata_startup_script = file("scripts/startup-script.ps1")
+
+  network_interface {
+    network       = google_compute_network.vpc.name
+    subnetwork    = google_compute_subnetwork.network_subnet.name
+    network_ip    = "10.10.15.4"
+    access_config { }
+  }
+} 
